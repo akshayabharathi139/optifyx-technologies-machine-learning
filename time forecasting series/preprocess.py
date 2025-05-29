372619666f7a18077948347d6e96dcf7dd2a12cb
Akshaya Bharathi <akshayabharathi139@gmail.com>
2025-05-29 16:12:35 +0530

    Delete time forecasting series directory

--- time forecasting series/preprocess.py
+++
@@ -1,9 +0,0 @@
-import pandas as pd
-
-def load_data(path):
-    df = pd.read_csv(path, parse_dates=['Date'])
-    df.set_index('Date', inplace=True)
-    return df
-
-def get_close_prices(df):
-    return df[['Close']]
--- time forecasting series/utils.py
+++
@@ -1,6 +0,0 @@
-import pandas as pd
-
-def load_data(filepath: str) -> pd.DataFrame:
-    df = pd.read_csv(filepath, parse_dates=['Date'])
-    df = df.sort_values('Date').reset_index(drop=True)
-    return df
